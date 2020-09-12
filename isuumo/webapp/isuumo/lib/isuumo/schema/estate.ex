191bee30290e41b1a723ec03bc9e5ff1d0d2d070
tamanugi <tamanugi@gmail.com>
2020-09-12 18:24:12 +0900

    fix estate

---
+++ isuumo/webapp/isuumo/lib/isuumo/schema/estate.ex
@@ -0,0 +1,17 @@
+defmodule Isuumo.Estate do
+  use Ecto.Schema
+
+  schema "estate" do
+    field(:name, :string)
+    field(:description, :string)
+    field(:thumbnail, :string)
+    field(:address, :string)
+    field(:latitude, :float)
+    field(:longitude, :float)
+    field(:rent, :integer)
+    field(:door_height, :integer)
+    field(:door_width, :integer)
+    field(:features, :string)
+    field(:popularity, :integer)
+  end
+end
